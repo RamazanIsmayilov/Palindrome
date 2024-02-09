bfea66f96ef44e8d9db9661f6b61e6c4d6946cde
Ramazan <ismayiloff522@gmail.com>
2024-02-10 00:15:35 +0400

    palindrome commit

---
+++ js/script.js
@@ -0,0 +1,22 @@
+const input = document.querySelector('input');
+const form = document.querySelector('form');
+const output = document.querySelector('.output');
+
+
+const checkPalindrome = (e) => {
+    e.preventDefault();
+    const text = input.value;
+    const reverseText = text.split('').reverse().join('');
+    if (!text) {
+        alert('Please input a value');
+    }else {
+        if(text === reverseText) {
+            output.innerHTML = text + " a palindrome.";
+        }else{
+            output.innerHTML = text + " is not a palindrome.";
+        }
+    }
+    input.value = ""
+}
+
+form.onsubmit = checkPalindrome;
